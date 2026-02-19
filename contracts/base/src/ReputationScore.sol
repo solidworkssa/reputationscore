babd3c3b37e6db5959271c7bcba05970d3f5650f
solidworkssa <solidworkssa@gmail.com>
2026-02-19 05:38:32 +0100

    fix: add author attribution and version to ReputationScore contract

--- contracts/base/src/ReputationScore.sol
+++ contracts/base/src/ReputationScore.sol
@@ -2,8 +2,11 @@
 pragma solidity ^0.8.20;
 
 /// @title ReputationScore Contract
+/// @author solidworkssa
 /// @notice On-chain reputation tracking system.
 contract ReputationScore {
+    string public constant VERSION = "1.0.0";
+
 
     mapping(address => uint256) public scores;
     address public admin;
